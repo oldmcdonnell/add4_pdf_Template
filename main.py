e9e70cdea55be67081bc2505515908869b95548f
oldmcdonnell <oldmcdonnell@gmail.com>
2023-11-22 17:34:14 -0500

    added lines to the document as a test

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"],
          align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
-
+    for x in range(20, 280, 10):
+        pdf.line(10, x, 200, x)
     #set the footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -25,11 +25,14 @@ for index, row in df.iterrows():
     for i in range(row['Pages'] -1):
         pdf.add_page()
 
+        #write the lines
+        for x in range(20, 280, 10):
+            pdf.line(10, x, 200, x)
+
         #Set the footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
-
 
 pdf.output("output.pdf")
